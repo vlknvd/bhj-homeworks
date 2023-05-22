49b05f2ee9d16d1b3f82ae8ceb9e59816ef8ef89
Darya <vlknvd@yandex.ru>
2023-05-22 17:35:46 +0300

    Change ABS

--- dom/ads/task.js
+++ dom/ads/task.js
@@ -1,15 +1,19 @@
 const rotator = document.querySelector('.rotator__case');
+const parentRotator = document.querySelector('.rotator');
+const activeRotator = document.querySelector('.rotator__case_active')
 let currentRotator = rotator;
 let previousRotator = rotator;
 
 setInterval(() => {
-    if (currentRotator.classList.contains('rotator__case_active')) {
+    currentRotator === activeRotator;
+    currentRotator.classList.remove('rotator__case_active');
+
+    if(currentRotator.nextElementSibling) {
         currentRotator = currentRotator.nextElementSibling;
-        previousRotator = currentRotator.previousElementSibling;
-        if(currentRotator.nextElementSibling === null) {
-            currentRotator = rotator;
-        }
-        currentRotator.classList.add('rotator__case_active');
-        previousRotator.classList.remove('rotator__case_active');
-}
+    } else {
+        currentRotator = parentRotator.firstElementChild;
+        previousRotator.classList.remove('rotator__case_active')
+    }
+
+    currentRotator.classList.add('rotator__case_active');
 }, 1000);
